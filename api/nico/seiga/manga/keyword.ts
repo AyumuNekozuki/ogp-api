0374ee0dd0cc4ea981bec524c1ba8a12c3145e6f
AyumuNekozuki <ayumu@nekozuki.me>
2022-07-07 19:20:05 +0900

    漫画検索API作成

---
+++ api/nico/seiga/manga/keyword.ts
@@ -0,0 +1,133 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+/**
+ * OGPタグを取得して、そのcontentをJSON形式で返す.
+ * 使用例:
+ *    endpoint/api/ogp?url="サイトのURL"
+ *
+ * @param req HTTP request
+ * @param res HTTP responce
+ */
+export default async function (req: NowRequest, res: NowResponse) {
+  const keyword = encodeURI(req.query.q + '');
+  const _offset = req.query._offset;
+  // const target = req.query.target || 'illust_all';
+
+  const _sort_query = req.query._sort;
+  let _sort_order :string = '';
+  switch (_sort_query) {
+    case '-viewCounter':
+      _sort_order = 'manga_view';
+      break;
+    case '+viewCounter':
+      _sort_order = 'manga_view_a';
+      break;
+    case '-commentCounter':
+      _sort_order = 'comment_count';
+      break;
+    case '+commentCounter':
+      _sort_order = 'comment_count_a';
+      break;
+  
+    default:
+      break;
+  }
+
+  if (!keyword) {
+    errorResponce(res);
+    return;
+  }
+
+  const _offset_num: number = Number(_offset) / 20;
+
+  const urls = [
+    `https://seiga.nicovideo.jp/manga/search/${keyword}?page=` + calc_sum(_offset_num, 1) +`&sort=${_sort_order}`,
+    `https://seiga.nicovideo.jp/manga/search/${keyword}?page=` + calc_sum(_offset_num, 2) +`&sort=${_sort_order}`,
+    `https://seiga.nicovideo.jp/manga/search/${keyword}?page=` + calc_sum(_offset_num, 3) +`&sort=${_sort_order}`,
+    `https://seiga.nicovideo.jp/manga/search/${keyword}?page=` + calc_sum(_offset_num, 4) +`&sort=${_sort_order}`,
+    `https://seiga.nicovideo.jp/manga/search/${keyword}?page=` + calc_sum(_offset_num, 5) +`&sort=${_sort_order}`,
+  ]
+
+
+  try {
+    const responce = await Promise.all(urls.map(url => axios.get(url)));
+    const data = responce.map(res => res.data);
+    const doms = data.map(data => new JSDOM(data));
+    let datas: object[] = [];
+    let totalcount: string = '';
+
+    let counter:number = 0;
+    doms.forEach(dom => {
+
+      if(counter == 0){
+        totalcount = dom.window.document.querySelectorAll('.header__result-summary')[0].textContent || '-';
+        totalcount = totalcount.replace(/[^0-9]/g, '');
+        counter++;
+      }
+
+      if(dom.window.document.querySelectorAll('.search_result .search_result__item')){
+        var items = dom.window.document.querySelectorAll('.search_result .search_result__item');
+        items.forEach(item => {
+
+          // コンテンツID
+          var url:string = item.querySelector('.search_result__item__info--title a').getAttribute('href');
+          var content_id:string = url.substring(url.indexOf('comic/'));
+          content_id = content_id.substring(0, content_id.indexOf('?'));
+
+          // カウンター
+          var mylistCounter:string = item.querySelector('.search_result__item__info--others-favorited').textContent;
+          mylistCounter = mylistCounter.replace(/[^0-9]/g, '');
+
+          // アップロード
+          var upload_date:string = item.querySelectorAll('.search_result__item__info--others-last_updated time')[0].getAttribute('datetime');
+
+          var contentsdata: object = {
+            contentId: content_id,
+            title: item.querySelectorAll('.search_result__item__thumbnail img')[0].getAttribute('alt'),
+            thumbnailUrl: item.querySelectorAll('.search_result__item__thumbnail img')[0].getAttribute('data-original'),
+            viewCounter: undefined,
+            commentCounter: undefined,
+            mylistCounter: mylistCounter,
+            startTime: upload_date,
+          }
+          datas.push(contentsdata);
+        })
+      }
+    })
+
+    const responsedata: object = {
+      meta: {
+        status: 200,
+        totalcount: totalcount,
+      },
+      data: datas, 
+    }
+
+    res.status(200).json(responsedata);
+  } catch (e) {
+    console.log(e)
+    errorResponce(res);
+  }
+}
+
+function isValidUrlParameter(url: string | string[]): boolean {
+  return !(url == undefined || url == null || Array.isArray(url));
+}
+
+function getUrlParameter(req: NowRequest): string | null {
+  const { url } = req.query;
+  if (isValidUrlParameter(url)) {
+    return <string>url;
+  }
+  return null;
+}
+
+function errorResponce(res: NowResponse): void {
+  res.status(400).send("error");
+}
+
+function calc_sum(a:number, b:number){
+  return a + b;
+}
