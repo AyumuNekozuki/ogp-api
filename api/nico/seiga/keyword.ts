1974ffc6e233b7845417a8b98612c354502b5fba
AyumuNekozuki <ayumu@nekozuki.me>
2022-07-07 12:27:38 +0900

    静画 キーワード検索api

--- api/nico/seiga/keyword.ts
+++ api/nico/seiga/keyword.ts
@@ -12,31 +12,64 @@ import { JSDOM } from "jsdom";
  */
 export default async function (req: NowRequest, res: NowResponse) {
   const keyword = req.query.q;
-  console.log(keyword)
+  const _offset = req.query._offset;
   if (!keyword) {
     errorResponce(res);
     return;
   }
 
-  const url = 'https://seiga.nicovideo.jp/search/' + keyword;
+  const _offset_num: number = Number(_offset) / 20;
+
+  const urls = [
+    'https://seiga.nicovideo.jp/search/' + keyword + '?page=' + _offset_num,
+    'https://seiga.nicovideo.jp/search/' + keyword + '?page=' + calc_sum(_offset_num, 1),
+    'https://seiga.nicovideo.jp/search/' + keyword + '?page=' + calc_sum(_offset_num, 2),
+    'https://seiga.nicovideo.jp/search/' + keyword + '?page=' + calc_sum(_offset_num, 3),
+    'https://seiga.nicovideo.jp/search/' + keyword + '?page=' + calc_sum(_offset_num, 4),
+  ]
+
 
   try {
-    const responce = await axios.get(<string>url);
-    const data = responce.data;
-    const dom = new JSDOM(data);
-    console.log(dom)
-    const meta = dom.window.document.querySelectorAll("head > meta");
+    const responce = await Promise.all(urls.map(url => axios.get(url)));
+    const data = responce.map(res => res.data);
+    const doms = data.map(data => new JSDOM(data));
+    let datas: object[] = [];
+    doms.forEach(dom => {
 
-    // metaからOGPを抽出し、JSON形式に変換する
-    const ogp = Array.from(meta)
-      .filter((element) => element.hasAttribute("property"))
-      .reduce((pre, ogp) => {
-        const property = ogp.getAttribute("property").trim().replace("og:", "");
-        const content = ogp.getAttribute("content");
-        pre[property] = content;
-        return pre;
-      }, {});
-    res.status(200).json(ogp);
+      var items = dom.window.document.querySelectorAll('.illust_pict_all .illust_list_img');
+      items.forEach(item => {
+
+        // コンテンツID
+        var url:string = item.querySelector('.center_img_inner ').getAttribute('href');
+        var content_id:string = url.substring(url.indexOf('im'));
+        content_id = content_id.substring(0, content_id.indexOf('?'));
+
+
+        // カウンター
+        var coutnertext: string = item.querySelectorAll('.counter_info')[0].textContent;
+        var viewCounter:string = coutnertext.substring(coutnertext.indexOf('閲覧：') + 3);
+        viewCounter = viewCounter.substring(0, viewCounter.indexOf(' '));
+       
+        var commentCounter:string = coutnertext.substring(coutnertext.indexOf('コメ：') + 3);
+        commentCounter = commentCounter.substring(0, commentCounter.indexOf(' '));
+
+        var mylistCounter:string = coutnertext.substring(coutnertext.indexOf('クリップ：') + 5);
+        mylistCounter = commentCounter.substring(0, commentCounter.indexOf(' '));
+
+        var contentsdata: object = {
+          contentId: content_id,
+          title: item.querySelectorAll('.illust_title a')[0].textContent,
+          thumbnailUrl: item.querySelectorAll('.center_img_inner img')[0].getAttribute('src'),
+          viewCounter: viewCounter,
+          commentCounter: commentCounter,
+          mylistCounter: mylistCounter,
+        }
+        datas.push(contentsdata);
+      })
+
+    })
+
+    res.status(200).json(datas);
   } catch (e) {
     errorResponce(res);
   }
@@ -56,4 +89,8 @@ function getUrlParameter(req: NowRequest): string | null {
 
 function errorResponce(res: NowResponse): void {
   res.status(400).send("error");
+}
+
+function calc_sum(a:number, b:number){
+  return a + b;
 }
